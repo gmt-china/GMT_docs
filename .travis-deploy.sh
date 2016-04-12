97a2c1ffd376ec4d703828b2141077c759884649
Dongdong Tian <seisman.info@gmail.com>
2016-04-12 11:35:23 +0800

    脚本通用化

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -8,20 +8,28 @@
 #  TRAVIS_PULL_REQUEST:
 #   - is a pull request => pull request number
 #   - not a pull request => false
+name=Dongdong Tian
+mail=seisman.info@gmail.com
+docdir=doc-dev
+docname=GMT_docs-dev
+pdfname=GMT_docs.pdf
 
 if [ ${TRAVIS_BRANCH} = "master" -a ${TRAVIS_PULL_REQUEST} = 'false' ]; then
     echo "In master branch, deploying now..."
-    git config user.name "Dongdong Tian"
-    git config user.email "seisman.info@gmail.com"
+    git config user.name "${name}"
+    git config user.email "${mail}"
+
     # Deploy Github Pages
     ghp-import -b gh-pages -n build/html -m 'Update by travis automatically'
     git push "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages --force
+
     # Deploy offline HTML and PDF files
-    mkdir build/doc-dev && cd build
-    mv html GMT_docs-dev && zip -r doc-dev/GMT_docs-dev.zip GMT_docs-dev
-    cp latex/GMT_docs.pdf doc-dev/GMT_docs-dev.pdf
-    ghp-import -b doc-dev doc-dev -m 'Update by travis automatically'
-    git push "https://${GH_TOKEN}@${GH_REF}" doc-dev:doc-dev --force
+    mkdir build/${docdir} && cd build
+    mv html ${docname} && zip -r ${docdir}/${docname}.zip ${docname}
+
+    cp latex/${pdfname} ${docdir}/${docname}.pdf
+    ghp-import -b ${docdir} ${docdir} -m 'Update by travis automatically'
+    git push "https://${GH_TOKEN}@${GH_REF}" ${docdir}:${docdir} --force
 else
     echo "Not in master branch"
 fi
