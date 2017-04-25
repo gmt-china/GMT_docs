66361a2f80d93b005286fe571de062afd7dfab8c
Dongdong Tian <seisman.info@gmail.com>
2017-04-25 14:01:59 +0800

    fix

--- scripts/GMT_CPT_3.sh
+++ scripts/GMT_CPT_3.sh
@@ -3,8 +3,8 @@ PS=GMT_CPT_3.ps
 
 gmt makecpt -Cglobe -T-8000/3000 > t.cpt
 
-gmt psscale -Ct.cpt -Baf -Dx0/0+w4.5i/0.1i+h -W0.001 -K -P > $PS
-gmt psscale -Cglobe -Baf -Dx0/0+w4.5i/0.1i+h -W0.001 -Y0.5i  -O >> $PS
+gmt psscale -Ct.cpt -Baf -Dx0/0+w4.5i/0.1i+h -K -P > $PS
+gmt psscale -Cglobe -Baf -Dx0/0+w4.5i/0.1i+h -Y0.5i  -O >> $PS
 
 rm gmt.*
 rm t.cpt
