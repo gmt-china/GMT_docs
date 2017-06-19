2ed6ff03b3a8b1d6a4d2440fd50c728bf5f9d950
Dongdong Tian <seisman.info@gmail.com>
2017-06-19 19:06:19 +0800

    GMT 5.4.1 no longer keep PSL_standard_fonts in share directory

--- scripts/GMT_fonts.sh
+++ scripts/GMT_fonts.sh
@@ -3,7 +3,7 @@ dy=-0.2222
 y0=4.3
 ps=GMT_fonts.ps
 
-grep -v '^#' `gmt --show-sharedir`/postscriptlight/PSL_standard_fonts.txt | awk '{print $1}' > tt.d
+grep -v '^#' PSL_standard_fonts.txt | awk '{print $1}' > tt.d
 gmt gmtset MAP_FRAME_PEN thinner
 gmt psxy -R0/5.4/0/$y0 -Jx1i -P -K -B0 <<EOF > $ps
 >
