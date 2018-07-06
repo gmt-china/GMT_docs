a7458a6b53ddfe2ee6b8f803da90c081eab905e6
Dongdong Tian <seisman.info@gmail.com>
2018-07-06 20:29:57 +0800

    update -U option

--- source/scripts/GMT_-U.sh
+++ source/scripts/GMT_-U.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gmt psbasemap -R0/10/0/5 -JX10c/3c -Bx1 -By1 -P -UBL/-1.5c/-1.5c/"This is a GMT logo" > GMT_-U.ps
-rm gmt.*
+gmt begin GMT_-U pdf,png
+gmt basemap -R0/8/0/3 -Jx1c -Bx1 -By1 -UBL/-1.5c/-1.5c/"This is a GMT logo"
+gmt end
