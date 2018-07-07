c39a3cab7b9c9025bf6dfb2df1efa0d69e132e99
Dongdong Tian <seisman.info@gmail.com>
2018-07-07 22:07:15 +0800

    update XY.rst

--- source/scripts/GMT_-XY.sh
+++ source/scripts/GMT_-XY.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-PS=GMT_-XY.ps
 J=X5c/2c
 R=0/5/0/2
 
-gmt psbasemap -J$J -R$R -B1 -K > $PS
-gmt pstext -J$J -R$R -F+f40p -K -O >> $PS << EOF
+gmt begin GMT_-XY pdf,png
+
+gmt basemap -JX5c/2c -R0/5/0/2 -B1
+gmt text -F+f40p << EOF
 2.5 1 1
 EOF
-gmt psbasemap -J$J -R$R -B1 -K -O -X7c >> $PS
-gmt pstext -J$J -R$R -F+f40p -K -O >> $PS << EOF
+
+gmt basemap -B1 -X7c
+gmt text -F+f40p << EOF
 2.5 1 2
 EOF
-gmt psbasemap -J$J -R$R -B1 -K -O -X-7c -Y4c >> $PS
-gmt pstext -J$J -R$R -F+f40p -K -O >> $PS << EOF
+
+gmt basemap -B1 -X-7c -Y4c
+gmt pstext -F+f40p << EOF
 2.5 1 3
 EOF
-gmt psbasemap -J$J -R$R -B1 -K -O -X7c >> $PS
-gmt pstext -J$J -R$R -F+f40p -O >> $PS << EOF
+
+gmt basemap -B1 -X7c
+gmt text -F+f40p << EOF
 2.5 1 4
 EOF
-rm gmt.*
+
+gmt end
