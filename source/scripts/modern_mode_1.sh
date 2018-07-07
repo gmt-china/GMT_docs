60fb318d92776b27031b34fd1ab045ca303f10db
Dongdong Tian <seisman.info@gmail.com>
2018-07-07 16:21:11 +0800

    modern mode

---
+++ source/scripts/modern_mode_1.sh
@@ -0,0 +1,12 @@
+# 开始绘图，文件名为map，图片格式为PDF和PNG格式
+gmt begin map pdf,png
+    # 不再需要 -R -J -K -O 和重定向
+    gmt grdgradient -Nt0.2 -A45 data.nc -Gintens.nc
+    gmt makecpt -Cgeo -T-8000/2000 > t.cpt
+    gmt grdimage -Ct.cpt -Iintens.nc data.nc -JM6i -P
+    gmt coast -Rdata.nc -Dh -Baf -W0.75p
+    echo "Japan Trench" | gmt pstext -F+f32p+cTC -Dj0/0.2i -Gwhite
+    gmt plot -W2p lines.txt
+    gmt colorbar -DjBL+w3i/0.1i+h+o0.3i/0.4i -Ct.cpt -W0.001 -F+gwhite+p0.5p -Bxaf -By+l"km"
+# 绘图结束，生成图片文件 map.pdf 和 map.png
+gmt end
