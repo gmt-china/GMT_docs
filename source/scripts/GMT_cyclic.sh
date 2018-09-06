68d0b42a63950e0e334b06a29993d2331fdbf2d8
Dongdong Tian <seisman.info@gmail.com>
2018-09-06 13:43:31 -0400

    update cpt

---
+++ source/scripts/GMT_cyclic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ps=GMT_cyclic.ps
+gmt makecpt -T0/100 -Cjet -Ww > t.cpt
+gmt psbasemap -R0/20/0/1 -JM5i -BWse -P -K -Baf > $ps
+gmt psscale -Ct.cpt -R -J -O -K -Baf -DJBC >> $ps
+gmt psxy -R -J -O -T >> $ps
+rm gmt.history t.cpt
