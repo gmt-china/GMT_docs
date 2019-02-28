50b4d1265f80db913d4a9b340bc58c7567778ea3
Dongdong Tian <seisman.info@gmail.com>
2019-02-28 12:35:39 -0500

    Update to modern mode

--- source/gallery/beachball/beachball_2.sh
+++ source/gallery/beachball/beachball_2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-gmt psmeca -JQ104/15c -R102.5/105.5/30.5/32.5 -Ba -Sa1c -M > beachball_2.ps << EOF
+gmt meca -JQ104/15c -R102.5/105.5/30.5/32.5 -Ba -Sa1c -M -png beachball_2 << EOF
 # 经度 纬度 深度(km) strike dip rake 震级 newX newY ID
 104.33 31.91 39.8 32 64 85 7 0 0 A
 104.11 31.52 27.1 22 53 57 6 0 0 B
 EOF
-rm gmt.*
