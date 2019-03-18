74fe534181d5f923ac212edb903d46548f46dbe0
Dongdong Tian <seisman.info@gmail.com>
2019-03-18 18:43:33 -0400

    macOS can plot vertical Chinese text

---
+++ source/install/GMT_Chinese_macOS.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+gmt begin GMT_Chinese_macOS pdf,png
+gmt set FONT_TITLE 25p,41,black
+gmt set FONT_LABEL 15p,39,black
+
+gmt text -R0/8/0/4 -JX12c/4c -Bxaf+l"X轴" -Byaf+l"Y轴" -BWSen+t"中文标题" -F+f << EOF
+2 3.5 25p,39,black 宋体横排
+2 2.5 25p,40,blue  仿宋横排
+2 1.5 25p,41,red   黑体横排
+2 0.5 25p,42,green 楷体横排
+4 3.5 25p,43,black 宋体竖排
+5 3.5 25p,44,blue  仿宋竖排
+6 3.5 25p,45,red   黑体竖排
+7 3.5 25p,46,green 楷体竖排
+EOF
+gmt end
