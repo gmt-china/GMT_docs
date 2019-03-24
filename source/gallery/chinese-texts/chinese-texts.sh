3b9f4a245cddd824d0e11e38954a4b39a4a85fcb
Dongdong Tian <seisman.info@gmail.com>
2019-03-24 15:24:01 -0400

    Fix vertical Chinese texts

--- source/gallery/chinese-texts/chinese-texts.sh
+++ source/gallery/chinese-texts/chinese-texts.sh
@@ -7,9 +7,18 @@ gmt text -F+f << EOF
 8 4.0 20p,43,red   竖排文字
 EOF
 
+# 中图
+gmt set FONT_TITLE 24p,41,black FONT_LABEL 16p,39,black
+gmt basemap -R0/10/0/5 -JX6c/4c -Bxaf+l'横轴' -Byaf+L'纵轴' -BWSen+t'中文标题' -X9c
+gmt text -F+f << EOF
+3 2.5 20p,39,black 横排文字
+8 4.0 20p,43,red   竖排文字
+EOF
+
 # 右图
 gmt set FONT_TITLE 24p,41,black FONT_LABEL 16p,39,black
-gmt basemap -R0/10/0/5 -JX6c/4c -Bxaf+l'横轴' -Byaf+L'纵轴' -BWSen+t'中文标题' -X10c
+gmt basemap -R0/10/0/5 -JX6c/4c -Bxaf+l'横轴' -BWSen+t'中文标题' -X9c
+gmt basemap -Byaf+L'纵轴' -BW --FONT_LABEL=16p,43,black --MAP_LABEL_OFFSET=20p
 gmt text -F+f << EOF
 3 2.5 20p,39,black 横排文字
 8 4.0 20p,43,red   竖排文字
