8b19602534037f1e5e2b7d9d958b7832cc1c10d2
Dongdong Tian <seisman.info@gmail.com>
2019-03-25 19:08:41 -0400

    GMT plus symbol issue fixed

--- source/conf/map_frame_type.sh
+++ source/conf/map_frame_type.sh
@@ -6,7 +6,7 @@ gmt subplot begin 1x5 -Fs4c/4c -M0.5c/0.2c
 
 gmt basemap -JM -R0/10/0/10 -Baf -B+t"fancy" -c1,1 --MAP_FRAME_TYPE=fancy
 
-gmt basemap -Baf -B+t"fancy\053" -c1,2 --MAP_FRAME_TYPE=fancy+
+gmt basemap -Baf -B+t"fancy+" -c1,2 --MAP_FRAME_TYPE=fancy+
 
 gmt basemap -JX -R0/10/0/10 -Bxaf -BWSen+t"plain" -c1,3 --MAP_FRAME_TYPE=plain
 
