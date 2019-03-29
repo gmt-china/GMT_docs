52b7e6b010c4f9039262ad9e90c24b452377f625
Dongdong Tian <seisman.info@gmail.com>
2019-03-29 08:56:10 -0400

    Revert "Cache to speedup"
    This reverts commit 89de91fb396367e78589310de5e194c3141ea76a.

--- ci/install-gmt.sh
+++ ci/install-gmt.sh
@@ -5,30 +5,30 @@
 set -e
 
 # Basic Information
-GSHHG_VERSION=2.3.7
-DCW_VERSION=1.1.4
-GMT_VERSION=5.4.5
+GMT_INSTALL=${HOME}/GMT-${GMT_VERSION}
 
-mkdir -p /tmp/GMT-build/
-cd /tmp/GMT-build
-
-# Download files
+# download files
 if [[ "${GMT_VERSION}" != '6.0.0' ]]; then
-    wget --quiet https://github.com/GenericMappingTools/gmt/archive/${GMT_VERSION}.tar.gz -O gmt.tar.gz
+    wget --quiet https://github.com/GenericMappingTools/gmt/archive/${GMT_VERSION}.tar.gz -O gmt-${GMT_VERSION}-src.tar.gz
 else
-    wget --quiet https://github.com/GenericMappingTools/gmt/archive/master.tar.gz -O gmt.tar.gz
+    wget --quiet https://github.com/GenericMappingTools/gmt/archive/master.tar.gz -O gmt-${GMT_VERSION}-src.tar.gz
 fi
-wget --quiet http://www.soest.hawaii.edu/pwessel/gshhg/gshhg-gmt-${GSHHG_VERSION}.tar.gz -O gshhg-gmt.tar.gz
-wget --quiet http://www.soest.hawaii.edu/pwessel/dcw/dcw-gmt-${DCW_VERSION}.tar.gz -O dcw-gmt.tar.gz
+wget --quiet http://www.soest.hawaii.edu/pwessel/gshhg/gshhg-gmt-${GSHHG_VERSION}.tar.gz
+wget --quiet http://www.soest.hawaii.edu/pwessel/dcw/dcw-gmt-${DCW_VERSION}.tar.gz
 
 # Now start to install
-tar -xf gmt.tar.gz --strip-components=1
-mkdir share/gmt-gshhg && tar -xf gshhg-gmt.tar.gz -C share/gmt-gshhg --strip-components=1
-mkdir share/gmt-dcw && tar -xf dcw-gmt.tar.gz -C share/gmt-dcw --strip-components=1
-rm gmt.tar.gz gshhg-gmt.tar.gz dcw-gmt.tar.gz
+mkdir gmt-${GMT_VERSION}
+tar xf gmt-${GMT_VERSION}-src.tar.gz -C gmt-${GMT_VERSION} --strip-components=1
+tar -xf gshhg-gmt-${GSHHG_VERSION}.tar.gz
+tar -xf dcw-gmt-${DCW_VERSION}.tar.gz
+
+mv gshhg-gmt-${GSHHG_VERSION} gmt-${GMT_VERSION}/share/gshhg
+mv dcw-gmt-${DCW_VERSION} gmt-${GMT_VERSION}/share/dcw-gmt
+
+cd gmt-${GMT_VERSION}
 
 cat > cmake/ConfigUser.cmake << EOF
-set (CMAKE_INSTALL_PREFIX "${GMT_INSTALL_PREFIX}")
+set (CMAKE_INSTALL_PREFIX "${GMT_INSTALL}")
 set (GMT_INSTALL_MODULE_LINKS FALSE)
 set (COPY_GSHHG TRUE)
 set (COPY_DCW TRUE)
@@ -40,9 +40,8 @@ cd build
 cmake ..
 make -j
 make -j install
-rm -r ${GMT_INSTALL_PREFIX}/share/doc
+cd ../..
 
-cd ${TRAVIS_BUILD_DIR}
-
+export PATH=${GMT_INSTALL}/bin:${PATH}
 # Turn off exit on failure.
 set +e
