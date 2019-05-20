cc75279e860faae800fd10d9de99835dba34c128
Dongdong Tian <seisman.info@gmail.com>
2019-05-19 23:14:01 -0400

    fix

--- ci/install-gmt-via-conda.sh
+++ ci/install-gmt-via-conda.sh
@@ -1,3 +1,5 @@
+# cleanup conda install path
+rm -rf ${CONDA_INSTALL_PREFIX}
 wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
 bash miniconda.sh -b -p ${CONDA_INSTALL_PREFIX}
