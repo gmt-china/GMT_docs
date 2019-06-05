b1d26431900fc8615b04756f02f0faf1d6a83626
Dongdong Tian <seisman.info@gmail.com>
2019-06-04 23:00:22 -0400

    Add histogram gallery (#125) (#127)

--- source/scripts/GMT_CPT_3.sh
+++ source/scripts/GMT_CPT_3.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 gmt begin cpt_3 pdf,png
-gmt makecpt -Cglobe -T-8000/3000 > t.cpt
-
-gmt colorbar -Ct.cpt -Baf -Dx0/0+w4.5i/0.1i+h
+gmt makecpt -Cglobe -T-8000/3000
+gmt colorbar -C -Baf -Dx0/0+w4.5i/0.1i+h
 gmt colorbar -Cglobe -Baf -Dx0/0+w4.5i/0.1i+h -Y0.5i
-
-rm t.cpt
 gmt end
