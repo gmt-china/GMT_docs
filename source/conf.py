e978cf54d1a41f032b5fa1a57ce6f21a42445749
Dongdong Tian <seisman.info@gmail.com>
2019-06-11 10:45:02 -0400

    Fix theme settings

--- source/conf.py
+++ source/conf.py
@@ -63,7 +63,7 @@ html_search_language = 'zh'
 html_theme_options = {
     'canonical_url': 'https://docs.gmt-china.org/latest/',
     'prev_next_buttons_location': 'bottom',
-    'sticky_navigation': False,
+    'sticky_navigation': True,
 }
 html_context = {
     'display_github': True,
