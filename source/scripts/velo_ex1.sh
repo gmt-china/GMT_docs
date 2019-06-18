c970f88235ef5fc969a3568129bc45d7b1ad4e5b
wangliang <wangliang0222@gmail.com>
2019-06-18 23:44:59 +0800

    fix

--- source/scripts/velo_ex1.sh
+++ source/scripts/velo_ex1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ps=psvelo_ex1.ps
 
+gmt begin velo_ex1 pdf
 # 左边三个符号
-gmt psvelo << EOF -Xc -JM15c -R241/242/34/35 -B0.5 -BWeSn+tpsvelo \
-    -Sw3c/1.e7 -W1p -G60 -E210 -D2 -P -K > $ps
+gmt velo -JM15c -R241/242/34/35 -B0.5 -BWeSn+tvelo \
+        -Sw3c/1.e7 -W1p -G60 -E210 -D2 << EOF
 # lon     lat    spin(rad/yr) spin_sigma (rad/yr)
 241.2 34.2  5.65E-08 1.17E-08
 241.2 34.5 -4.85E-08 1.85E-08
@@ -11,11 +11,11 @@ gmt psvelo << EOF -Xc -JM15c -R241/242/34/35 -B0.5 -BWeSn+tpsvelo \
 EOF
 
 # 右边三个符号
-gmt psvelo -J -R -Sw3c/1.e7 -W1p -D2 -O -K << EOF >> $ps
+gmt velo -Sw3c/1.e7 -W1p -D2 << EOF
 # lon     lat    spin(rad/yr) spin_sigma (rad/yr)
 241.7 34.2  5.65E-08 1.17E-08
 241.7 34.5 -4.85E-08 1.85E-08
 241.7 34.7  4.46E-09 3.07E-08
 EOF
-gmt pscoast -O -R -J -W1p -Di >> $ps
-rm gmt.*
+gmt pscoast -W1p -Di
+gmt end
