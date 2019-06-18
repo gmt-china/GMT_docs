95fbd608d517d73d9558a1be7214bcddbf8007bb
王亮 <wangliang0222@gmail.com>
2019-06-18 23:55:59 +0800

    Update velo (#110)

--- source/scripts/velo_ex2.sh
+++ source/scripts/velo_ex2.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-ps=psvelo_ex2.ps
 
-gmt psvelo -R-10/10/-10/10 -Wthin,red \
-	-Se0.2c/0.39/12 -B1g1 -BWeSn -Jx0.2i -Ggreen -Eblue -L -N \
-	-A1c+p3p+e -K << EOF > $ps
+gmt begin velo_ex2 pdf
+gmt velo -R-10/10/-10/10 -Wthin,red -Se0.2c/0.39/12 -B1g1 \
+        -BWeSn -Jx0.2i -Ggreen -Eblue -L -N -A1c+p3p+e << EOF
 # Long.   Lat.   Evel   Nvel   Esig   Nsig  CorEN SITE
 # (deg)  (deg)    (mm/yr)        (mm/yr)
    -8.    0.     5.0    0.0     4.0    6.0  0.500  4x6
@@ -13,7 +12,7 @@ gmt psvelo -R-10/10/-10/10 -Wthin,red \
    -1.    5.     3.0    3.0     1.0    1.0  0.100  3x3
 EOF
 
-gmt psvelo -Se0.2c/0.39/14 -R -J -A0.25c+p0.25p+e -O << EOF >> $ps
+gmt velo -Se0.2c/0.39/14 -A0.25c+p0.25p+e << EOF
 # Long.   Lat.   Evel   Nvel   Esig   Nsig  CorEN SITE
 # (deg)  (deg)    (mm/yr)        (mm/yr)
    0.    -8.     0.0    0.0     4.0    6.0  0.100  4x6
@@ -23,4 +22,4 @@ gmt psvelo -Se0.2c/0.39/14 -R -J -A0.25c+p0.25p+e -O << EOF >> $ps
    5.     0.    -6.0    4.0     6.0    4.0 -0.300  -6x4
    0.    -5.     6.0   -4.0     6.0    4.0 -0.500  6x-4
 EOF
-rm gmt.*
+gmt end
