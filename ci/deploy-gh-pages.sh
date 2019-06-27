5c3666842c55dcf98fa178e4548f54ea6947a5b4
Dongdong Tian <seisman.info@gmail.com>
2019-06-27 12:42:17 -0400

    Update travis settings

--- ci/deploy-gh-pages.sh
+++ ci/deploy-gh-pages.sh
@@ -17,11 +17,7 @@ CLONE_ARGS="--quiet --branch=$BRANCH --single-branch"
 REPO_URL=https://${GH_TOKEN}@github.com/${REPO}.git
 HTML_SRC=${TRAVIS_BUILD_DIR}/${HTML_BUILDDIR:-doc/_build/html}
 # Place the HTML is different folders for different versions
-if [[ "${TRAVIS_TAG}" != "" ]]; then
-    VERSION=${TRAVIS_TAG}
-else
-    VERSION=${GMT_VERSION}
-fi
+VERSION=${GMT_VERSION}
 
 echo -e "DEPLOYING HTML TO GITHUB PAGES:"
 echo -e "Target: branch ${BRANCH} of ${REPO}"
@@ -43,35 +39,14 @@ git config user.name "TravisCI"
 echo -e "Remove old files from previous builds"
 rm -rf ${VERSION}
 cp -Rf ${HTML_SRC}/ ${VERSION}/
-# Another copy to root directory
-if [[ "${TRAVIS_BRANCH}" == 'master' ]]; then
-    # direct index.html to latest
-    echo "<meta http-equiv=\"Refresh\" content=\"0;url=latest/\"/>" > index.html
-fi
 
 # Need to have this file so that Github doesn't try to run Jekyll
 touch .nojekyll
 
-# If this is a new release, update the link from /latest to the new release
-if [[ "${TRAVIS_TAG}" != "" ]]; then
-    echo -e "Setup link from ${VERSION} to 'latest'"
-    rm -f latest
-    ln -sf ${VERSION} latest
-fi
-
 echo -e "Add and commit changes"
 git add -A .
 git status
-# If this is a dev build and the last commit was from a dev build, reuse the
-# same commit
-if [[ "${TRAVIS_TAG}" != "" && `git log -1 --format='%s'` == *"${GMT_VERSION}"* ]]; then
-    echo -e "Amending last commit"
-    git commit --amend --reset-author --no-edit
-else
-    # Make a new commit
-    echo -e "Making a new commit"
-    git commit -m "Deploy $VERSION from TravisCI"
-fi
+git commit -m "Deploy $VERSION from TravisCI"
 
 echo -e "Pushing..."
 git push -fq origin $BRANCH 2>&1 >/dev/null
