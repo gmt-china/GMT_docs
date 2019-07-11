13b47a454d804c2df9024d633ea4bc3c5c902564
spring <dengshanquan14@mails.ucas.edu.cn>
2019-07-11 21:01:32 +0800

    modernize text commands

--- source/scripts/pstext_-M.sh
+++ source/scripts/pstext_-M.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-gmt pstext -R0/3/0/5 -JX3i -F+f+a+j -B1 -M -N -Y4c << EOF > pstext_-M.ps
+gmt text -R0/3/0/5 -JX3i -F+f+a+j -B1 -M -N -Y4c -pdf pstext_-M << EOF
 > 0 -0.5 12p,black 0 LT 13p 3i j
 @%5%Figure 1.@%% This illustration shows nothing useful, but it still needs
 a figure caption. Highlighted in @;255/0/0;red@;; you can see the locations
 of cities where it is @_impossible@_ to get any good Thai food; these are to be avoided.
 EOF
-
-rm gmt.*
