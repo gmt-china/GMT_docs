7cf24e838f48effa40e44cc16157410ce499df7b
wangliang <wangliang0222@gmail.com>
2019-08-12 22:41:33 +0800

    modernize basemap

---
+++ source/scripts/basemap_ex3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Showing map directional roses
+gmt begin basemap_ex3 pdf,png
+gmt set FONT_LABEL 10p FONT_TITLE 12p MAP_ANNOT_OBLIQUE 34 MAP_TITLE_OFFSET 5p \
+	MAP_FRAME_WIDTH 3p FORMAT_GEO_MAP dddF FONT_ANNOT_PRIMARY 10p
+# left: Fancy kind = 1
+gmt basemap -R-5/5/-5/5 -Jm0.15i -Ba5f -BWSne+gazure1 -Tdg0/0+w1i+jCM -X1i
+# middle: Fancy kind = 3
+gmt basemap -Ba5f -BwSne+gazure1 -Tdg0/0+w1i+f1+jCM -X1.75i
+# right: Plain kind
+gmt basemap -R-7/7/-5/5 -J -Ba5f -BwSnE+gazure1 -Tdg0/0+w1i+f3+l+jCM -X1.75i
+gmt end
