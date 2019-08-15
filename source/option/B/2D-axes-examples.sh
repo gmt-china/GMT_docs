d5db68eda0b710ec74286e3ec7fc040a51656560
Dongdong Tian <seisman.info@gmail.com>
2019-08-14 22:55:03 -0400

    Fix two subplot examples [ci skip]

--- source/option/B/2D-axes-examples.sh
+++ source/option/B/2D-axes-examples.sh
@@ -1,9 +1,9 @@
 gmt begin 2D-axes-examples png,pdf
 gmt set FONT_TITLE 15p
 gmt subplot begin 1x7 -Fs4c/4c -M5p/10p
-    gmt basemap -R0/4/0/4 -BWS+t'-BWS' -c1,1
-    gmt basemap -R0/4/0/4 -BWSEN+t'-BWSEN' -c1,2
-    gmt basemap -R0/4/0/4 -BWSen+t'-BWSen' -c1,3
-    gmt basemap -R0/4/0/4 -BWSrt+t'-BWSrt' -c1,4
+    gmt basemap -R0/4/0/4 -BWS+t'-BWS' -c
+    gmt basemap -R0/4/0/4 -BWSEN+t'-BWSEN' -c
+    gmt basemap -R0/4/0/4 -BWSen+t'-BWSen' -c
+    gmt basemap -R0/4/0/4 -BWSrt+t'-BWSrt' -c
 gmt subplot end
 gmt end
--- source/option/B/3D-axes-examples.sh
+++ source/option/B/3D-axes-examples.sh
@@ -1,10 +1,10 @@
 gmt begin 3D-axes-examples png,pdf
 gmt set FONT_TITLE 15p
 gmt subplot begin 1x7 -Fs4c/4c -M20p/10p
-    gmt basemap -R0/10/0/10/0/10 -JX5c -JZ5c -Bxaf -Byaf -Bzaf -BwesnZ+t'-BwesnZ' -p130/30 -c1,1
-    gmt basemap -R0/10/0/10/0/10 -JX5c -JZ5c -Bxaf -Byaf -Bzaf -Bwesnz+t'-Bwesnz' -p130/30 -c1,2
-    gmt basemap -R0/10/0/10/0/10 -JX5c -JZ5c -Bxaf -Byaf -Bzaf -BwesnZ1234+t'-BwesnZ1234' -p130/30 -c1,3
-    gmt basemap -R0/10/0/10/0/10 -JX5c -JZ5c -Bxaf -Byaf -Bzaf -Bwesnz1234+t'-Bwesnz1234' -p130/30 -c1,4
-    gmt basemap -R0/10/0/10/0/10 -JX5c -JZ5c -Bxaf -Byaf -Bzaf -BwesnZ+b+t'-BwesnZ+b' -p130/30 -c1,5
+    gmt basemap -R0/10/0/10/0/10 -JX5c -JZ5c -Bxaf -Byaf -Bzaf -BwesnZ+t'-BwesnZ' -p130/30 -c
+    gmt basemap -R0/10/0/10/0/10 -JX5c -JZ5c -Bxaf -Byaf -Bzaf -Bwesnz+t'-Bwesnz' -p130/30 -c
+    gmt basemap -R0/10/0/10/0/10 -JX5c -JZ5c -Bxaf -Byaf -Bzaf -BwesnZ1234+t'-BwesnZ1234' -p130/30 -c
+    gmt basemap -R0/10/0/10/0/10 -JX5c -JZ5c -Bxaf -Byaf -Bzaf -Bwesnz1234+t'-Bwesnz1234' -p130/30 -c
+    gmt basemap -R0/10/0/10/0/10 -JX5c -JZ5c -Bxaf -Byaf -Bzaf -BwesnZ+b+t'-BwesnZ+b' -p130/30 -c
 gmt subplot end
 gmt end
