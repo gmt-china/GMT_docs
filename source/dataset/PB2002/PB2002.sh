9abae72a5dd9c28d2a8ea2648726a19b266010ca
Dongdong Tian <seisman.info@gmail.com>
2020-03-09 02:02:23 -0400

    Update PB2002 data

--- source/dataset/PB2002/PB2002.sh
+++ source/dataset/PB2002/PB2002.sh
@@ -1,4 +1,4 @@
 gmt begin PB2002 pdf,png
 gmt coast -JH20c -Rg -B0 -B+t'PB2002' -W1/0.1p,black -A1000
-gmt plot PB2002_plates.dig.txt -W0.5p,red
+gmt plot PB2002_boundaries.dig.txt -W0.5p,red
 gmt end show
