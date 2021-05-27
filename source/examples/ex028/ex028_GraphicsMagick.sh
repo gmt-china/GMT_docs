c153efeec9750ba8cd0983204056d83529983925
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2021-05-27 11:47:44 +0900

    fix

--- source/examples/ex028/ex028_GraphicsMagick.sh
+++ source/examples/ex028/ex028_GraphicsMagick.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gnss1=./gnss_b1_hlf_allppk.txt
+gnss1=gnss_b1_hlf_allppk.txt
 R=-R32400/119700/-0.565631/0.828242
 
 export GMT_SESSION_NAME=$$
@@ -7,28 +7,28 @@ export GMT_SESSION_NAME=$$
 # 最外层的for循环
 for((i=1;i<=23;i++));  
 do   
-	((name = $i + 100))
-	r=$(($RANDOM%255))
-	g=$(($RANDOM%255))
-	b=$(($RANDOM%255))
+    ((name = $i + 100))
+    r=$(($RANDOM%255))
+    g=$(($RANDOM%255))
+    b=$(($RANDOM%255))
 
-	((max =  $i * 3600))
-	((min = ($i-1) * 3600))
+    ((max =  $i * 3600))
+    ((min = ($i-1) * 3600))
 
-	# 每个循环内部绘制一帧图像
-	gmt begin a00$name png
-		gmt set FORMAT_DATE_MAP "o dd" FORMAT_CLOCK_MAP hh:mm
-		gmt basemap -R2019-03-31T09:00:00/2019-04-01T09:00:00/-0.565631/0.828242 -Bpxa4Hf1h -Bsxa1Df1D -Bpya0.4g0.2f0.2+l"WSSE(HPF)/m" -JX10c/4c -BWSne
-		gmt basemap -R2/20/1e-4/1e0 -JX-10cl/10cl -Bxa2f3g3+l"period:second" -Bya-1pg+l"power spectrum:m@+2@+/cycle/s" -BWNse -Y2.7i
-		
-		awk 'NR<'"$max"' && NR>'"$min"'  {print $1,$2}' $gnss1 | gmt spectrum1d -S256 -W --GMT_FFT=brenner -N -i1 -D1 > pow5.txt
-		awk ' NR<'"$max"'   {print $1,$2}' $gnss1 | gmt plot $R -JX10c/4c -W0.5p,lightgray -Y-2.7i
-		awk 'NR<'"$max"' && NR>'"$min"'  {print $1,$2}' $gnss1 | gmt plot -W0.5p,$r/$g/$b
-		gmt plot -R2/20/1e-4/1e0 -JX-10cl/10cl pow5.txt -W2.25p,$r/$g/$b -Y2.7i -i0,1
+    # 每个循环内部绘制一帧图像
+    gmt begin a00$name png
+        gmt set FORMAT_DATE_MAP "o dd" FORMAT_CLOCK_MAP hh:mm
+        gmt basemap -R2019-03-31T09:00:00/2019-04-01T09:00:00/-0.565631/0.828242 -Bpxa4Hf1h -Bsxa1Df1D -Bpya0.4g0.2f0.2+l"WSSE(HPF)/m" -JX10c/4c -BWSne
+        gmt basemap -R2/20/1e-4/1e0 -JX-10cl/10cl -Bxa2f3g3+l"period:second" -Bya-1pg+l"power spectrum:m@+2@+/cycle/s" -BWNse -Y2.7i
+        
+        awk 'NR<'"$max"' && NR>'"$min"'  {print $1,$2}' $gnss1 | gmt spectrum1d -S256 -W --GMT_FFT=brenner -N -i1 -D1 > pow5.txt
+        awk ' NR<'"$max"'   {print $1,$2}' $gnss1 | gmt plot $R -JX10c/4c -W0.5p,lightgray -Y-2.7i
+        awk 'NR<'"$max"' && NR>'"$min"'  {print $1,$2}' $gnss1 | gmt plot -W0.5p,$r/$g/$b
+        gmt plot -R2/20/1e-4/1e0 -JX-10cl/10cl pow5.txt -W2.25p,$r/$g/$b -Y2.7i -i0,1
 
-		gmt basemap -R0.05/0.5/1e-4/0.5 -JX10cl/10cl -Bxa2f3g3+l"Frequency:cycle/second" -BS
-	gmt end
-	rm pow5.txt
+        gmt basemap -R0.05/0.5/1e-4/0.5 -JX10cl/10cl -Bxa2f3g3+l"Frequency:cycle/second" -BS
+    gmt end
+    rm pow5.txt
 done
 gm convert -delay 64 -loop 0 *.png wave_spectrum.gif
 rm a00*.png
--- source/examples/ex028/index.rst
+++ source/examples/ex028/index.rst
@@ -8,19 +8,19 @@ GMT制作GIF动画
 
 ----
 
-本示例展示如何使用GMT制作GIF动画。这里以GNSS浮标观测的海面波动为例，
+本示例展示如何使用 GMT 制作 GIF 动画。这里以 GNSS 浮标观测的海面波动为例，
 希望展示波浪的时间变化规律。动画设置每一帧显示一个小时的波浪功率谱图像，
 不同帧使用不同的颜色表达。示例数据下载 :download:`gnss_b1_hlf_allppk.txt`\ 。
 
-请注意，GMT的动画功能需要另外安装GraphicsMagick组件。由于这个组件是非必须可选安装项目，
-建议参阅GMT安装指南的相关章节，确认本机是否已经安装。
+请注意，GMT 的动画功能需要另外安装 GraphicsMagick 组件。由于这个组件是非必须可选安装项目，
+建议参阅 GMT 安装指南的相关章节，确认本机是否已经安装。
 
-制作动画有两种方法，一种是利用Bash脚本语言的for循环，首先先绘制出每一帧的图像，
-然后再利用GraphicsMagick合成为GIF动画：
+制作动画有两种方法，一种是利用 Bash 脚本语言的 for 循环，首先先绘制出每一帧的图像，
+然后再利用 GraphicsMagick 合成为 GIF 动画：
 
 .. literalinclude:: ex028_GraphicsMagick.sh
 
-另一种则是利用GMT的 ``movie`` 模块，语法更为简洁：
+另一种则是利用 GMT 的 ``movie`` 模块，语法更为简洁：
 
 .. literalinclude:: ex028_movie.sh
 
