06fa7dc8026a965108e6a75e5fb930dc0b1f7d96
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2021-06-03 09:20:35 +0900

    Apply suggestions from code review
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- source/examples/ex029/ex029.sh
+++ source/examples/ex029/ex029.sh
@@ -18,7 +18,9 @@ gmt begin
         # 预处理1. 生成断层的三维采样 -R选项是给远程服务器DEM数据一个范围，避免下载全球数据后再采样
         gmt grdtrack -R101/108/35/42 -G@earth_relief_02m CN-faults.gmt > faults.xyz
         # 预处理2. 进入数据目录（~/.gmt/server/earth/earth_day）用gdal_translate裁剪出绘图区域的day.tif
-        gdal_translate -of GTIFF -projwin 101 42 108 35 ~/.gmt/server/earth/earth_day/earth_day_02m_p.tif day.tif
+        # 下载 earth_day_02_p 数据到当前目录
+        gmt which -Gl @earth_day_02m_p
+        gdal_translate -of GTIFF -projwin 101 42 108 35 earth_day_02m_p.tif day.tif
   
         # 绘图1. -G表示在DEM上贴卫星图片
         gmt grdview @earth_relief_02m -R101/108/35/42/-1000/5000 -JM10c -JZ4c -N-1000+ggray -Gday.tif -Qi -Ba -Bz -BwsENZ -p60/25   
