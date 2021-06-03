a0e8f0fdbfedd4929adaa073cb8465cce4416df0
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2021-06-03 12:46:55 +0900

    Apply suggestions from code review
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- source/examples/ex029/ex029.sh
+++ source/examples/ex029/ex029.sh
@@ -10,7 +10,7 @@ gmt begin
         gmt plot CN-faults.gmt -W2p,brown -p60/25/10000 -Ba -BWSen
         gmt basemap -TdjLT+w1.5c+l+o1c -p
         
-        # 3. 卫星图片 -p设置图层抬高到20000
+        # 3. 绘制卫星图片图层。-p 设置图层抬高到20000
         gmt grdimage @earth_day_02m -p60/25/20000 -Ba -BWSen
 
     gmt figure map3D_2 png
