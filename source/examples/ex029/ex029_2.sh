071fa75f5efc5e328de9e91fe6103ea9820d1268
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2021-06-04 12:28:07 +0900

    Apply suggestions from code review
    Co-authored-by: Yao Jiayuan <coreman.seism@gmail.com>

--- source/examples/ex029/ex029_2.sh
+++ source/examples/ex029/ex029_2.sh
@@ -7,7 +7,7 @@ gmt begin 3D_earth_relief_2 pdf,png
     gmt grdtrack -R101/108/35/42 -G@earth_relief_02m CN-faults.gmt > faults.xyz
 
     # 预处理2：截取区域卫星图片
-    # 下载 2弧分的卫星图片数据 earth_day_02m_p 到当前目录
+    # 下载 2 弧分的卫星图片数据 earth_day_02m_p 到当前目录
     gmt which -Gl @earth_day_02m_p
     # 使用 gdal_translate 截取区域的卫星图片
     # -projwin 选项后的四个参数指定了区域范围。四个值分别是：
