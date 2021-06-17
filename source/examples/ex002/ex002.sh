243cb1cf746104fb7843e02f60c5bb0bff49c8e2
Dongdong Tian <seisman.info@gmail.com>
2021-06-17 11:16:56 -0400

    Update ex002.sh for GMT 6.2.0

--- source/examples/ex002/ex002.sh
+++ source/examples/ex002/ex002.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-gmt begin earth-discontinuities png
+gmt begin earth-discontinuities pdf,png
 gmt set MAP_GRID_PEN_PRIMARY 1p
 # 绘制地表
-gmt basemap -JP10c+a+t15 -R-10/40/0/6371 -Byg6371 -Bs
+gmt basemap -JP10c+a+t15 -R-10/40/0/6371 -Byg6371 -BS
 # 绘制 410 界面
-gmt basemap -Byg6371+5961 -Bs
+gmt basemap -Byg6371+5961 -BS
 # 绘制 660 界面
-gmt basemap -Byg6371+5711 -Bs
+gmt basemap -Byg6371+5711 -BS
 # 绘制 CMB
-gmt basemap -Byg6371+3480 -Bs
+gmt basemap -Byg6371+3480 -BS
 # 绘制 ICB
-gmt basemap -Byg6371+1221 -Bs
+gmt basemap -Byg6371+1221 -BS
 
 # 计算震相的射线路径，用户需安装 TauP，并取消以下注释
 # taup_path -mod prem -ph PcP -h 300 -deg 30 -o PcP.raypath
@@ -35,4 +35,4 @@ gmt text -F+f11p+a -N << EOF
 16 4100 0 @;blue;PcP@;;
 37 1600 0 @;red;PKiKP@;;
 EOF
-gmt end
+gmt end show
