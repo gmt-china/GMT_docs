3514792ab3b246d0d765f0a89e69a6a05fd46290
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2021-06-17 15:08:28 +0900

    Update ex011.sh

--- source/examples/ex011/ex011.sh
+++ source/examples/ex011/ex011.sh
@@ -13,15 +13,17 @@ J=E$stlo/$stla/90/10c
 gmt begin map png,pdf
 gmt set FORMAT_GEO_MAP=+D
 gmt coast -J$J -Rg -A10000 -Ggrey
+
 # 绘制台站位置（三角形）
-echo $stlo $stla | gmt plot -St0.4c -Gblue -Bya180
+echo $stlo $stla | gmt plot -St0.4c -Gblack -Bya180
 # 绘制地震震源位置
-gmt plot -Sa0.4c -Gyellow -W0.1p << EOF
+gmt plot -Sa0.25c -Gred -W0.1p << EOF
 145 30
 160 40
 60 40
 80 -10
 EOF
+
 # 绘制30和60度等震中距线
 echo $stlo $stla 6672 | gmt plot -SE- -W1p,red
 echo $stlo $stla 13344 | gmt plot -SE- -W1p,red
@@ -31,4 +33,5 @@ $stlo 0 30\232
 $stlo -30 60\232
 $stlo -59.9 90\232
 EOF
+
 gmt end show
