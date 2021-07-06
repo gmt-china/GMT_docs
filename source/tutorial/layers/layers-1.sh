86bbfd9beb5f6ae55358e49eb625f08a217f7af7
Mao Zhou <1831326353@qq.com>
2021-07-06 12:58:18 +0800

    Using low resolution topography data in tutorial layer (#532)

--- source/tutorial/layers/layers-1.sh
+++ source/tutorial/layers/layers-1.sh
@@ -3,6 +3,6 @@ gmt begin layers png,pdf
 
 # 绘制地形起伏底图
 gmt basemap -JH180/15c -Rg -B0
-gmt grdimage @earth_relief_10m -Cetopo1 -I+d
+gmt grdimage @earth_relief_30m -Cetopo1 -I+d
 gmt colorbar -Bxa2000f+l"Elevation (m)"
 gmt end show
