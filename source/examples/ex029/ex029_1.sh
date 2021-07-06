ae4fd7b476f6fdd1fa822c64b1233e2b3a88d4c8
maozhou <maozhou@whu.edu.cn>
2021-07-06 16:47:38 +0800

    Change resolution in 3D-topo(ex029)

--- source/examples/ex029/ex029_1.sh
+++ source/examples/ex029/ex029_1.sh
@@ -4,7 +4,7 @@
 gmt begin 3D_earth_relief_1 pdf,png
     gmt set MAP_FRAME_TYPE plain
     # 1. 绘制地形图形，图层默认位于 z 轴底部（此例中即 -1000 的高度）
-    gmt grdview @earth_relief_06m -R101/108/35/42/-1000/20000 -JM10c -JZ10c -N-1000+ggray -Qi -I -Ba -BwsENZ -p60/25    
+    gmt grdview @earth_relief_5m -R101/108/35/42/-1000/20000 -JM10c -JZ10c -N-1000+ggray -Qi -I -Ba -BwsENZ -p60/25    
     gmt colorbar -C -Ba -DJTC+o0/1c -p
 
     # 2. 绘制断层线图层，-p 设置图层抬高到10000 
@@ -12,5 +12,5 @@ gmt begin 3D_earth_relief_1 pdf,png
     gmt basemap -TdjLT+w1.5c+l+o1c -p
     
     # 3. 绘制卫星图片图层，-p 设置图层抬高到20000
-    gmt grdimage @earth_day_06m -p60/25/20000 -Ba -BWSen
+    gmt grdimage @earth_day_05m -p60/25/20000 -Ba -BWSen
 gmt end show
