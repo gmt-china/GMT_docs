ac75fa5dbcecdff3b08811ac39c224836586311f
maozhou <maozhou@whu.edu.cn>
2021-07-12 09:35:06 +0800

    fix

--- source/option/w/GMT_cycle_1.sh
+++ source/option/w/GMT_cycle_1.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 gmt begin GMT_cycle_1 pdf,png
     gmt set GMT_THEME cookbook
-    gmt plot @mississippi.txt -JX15cT/7c -Bxaf -Byaf+l"10@+3@+ m@+3@+/s" -BWSrt+t"Mississippi river daily discharge" -W0.25p,red -i0,1+s1e-3
+    gmt plot @mississippi.txt -JX15cT/7c -Bxaf -Byaf+l"10@+3@+ m@+3@+/s" 
+        -BWSrt+t"Mississippi river daily discharge" -W0.25p,red -i0,1+s1e-3
 gmt end show
--- source/option/w/GMT_cycle_3.sh
+++ source/option/w/GMT_cycle_3.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 gmt begin GMT_cycle_3 pdf,png
     gmt set GMT_THEME cookbook
-    gmt histogram @mississippi.txt -R-3/9/0/8 -JX15c/7c -T1 -Gred -W1p -Bxaf -Byaf+l"10@+6@+ m@+3@+/s" -BWSrt+t"Average monthly discharge" -Z0+w -i0,1+s1e-6 -wa
+    gmt histogram @mississippi.txt -R-3/9/0/8 -JX15c/7c -T1 -Gred -W1p -Bxaf -Byaf+l"10@+6@+ m@+3@+/s" \
+        -BWSrt+t"Average monthly discharge" -Z0+w -i0,1+s1e-6 -wa
 gmt end show
