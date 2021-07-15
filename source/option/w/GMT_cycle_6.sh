4ab70fa663b46882f421df24c0b8d0a1b172e01a
maozhou <maozhou@whu.edu.cn>
2021-07-15 10:53:45 +0800

    Add contributors and update scripts

--- source/option/w/GMT_cycle_6.sh
+++ source/option/w/GMT_cycle_6.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 gmt begin GMT_cycle_6 pdf,png
-  gmt set GMT_THEME cookbook
   gmt set TIME_WEEK_START Monday
   gmt subplot begin 2x2 -Fs15c/8c -M14p/6p -A \
     -T"Verrazzano-Narrows Bridge @%34%\337@%% Brooklyn Traffic [2018-2020]" -BWSrt
