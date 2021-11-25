24fb79ec63e0b90fcf5737fe3c1996af990c0ef7
王亮 <wangliang0222@gmail.com>
2021-11-25 11:10:58 +0800

    fix

--- source/examples/ex010/ex010.sh
+++ source/examples/ex010/ex010.sh
@@ -2,30 +2,23 @@
 
 R=70/135/15/55
 eqfile=eq.dat
-topodata=@earth_relief_06m
 
 gmt begin topo pdf,png
-gmt grdcut $topodata -R$R -GcutTopo.grd
-gmt grdgradient cutTopo.grd -Ne0.7 -A50 -GcutTopo_i.grd
-gmt grd2cpt cutTopo.grd -Cglobe -T-10000/10000/200 -Z -D
-#绘制底图
 gmt set FORMAT_GEO_MAP=ddd:mm:ssF
-gmt basemap -R70/135/15/55 -JM15c -Ba10f5 -BWesN
-gmt grdimage cutTopo.grd -IcutTopo_i.grd -Q
-gmt coast -W1/0.2p -I1/0.25p -N1/0.5p
+gmt grdimage @earth_relief_02m -R70/135/15/55 -JM15c -Ba10f5 -BWesN -I+d
 
 #绘制colorbar
 gmt colorbar -DjCB+w15c/0.5c+o0/-2c+h -Bxa2000f400+l"Elevation/m" -G-8000/8000
 
 #分震级绘制地震
-awk '$3>=5.0 && $3<6.0 {print $1,$2,$3*0.04}' $eqfile | gmt plot -Sc -Gblue
-M5=`awk '$3>=5.0 && $3<6.0' $eqfile | wc -l | cut -d" " -f1`
-awk '$3>=6.0 && $3<7.0 {print $1,$2,$3*0.04}' $eqfile | gmt plot -Sc -Gred
-M6=`awk '$3>=6.0 && $3<7.0' $eqfile | wc -l | cut -d" " -f1`
-awk '$3>=7.0 && $3<8.0 {print $1,$2,$3*0.04}' $eqfile | gmt plot -Sa -Ggreen
-M7=`awk '$3>=7.0 && $3<8.0' $eqfile | wc -l | cut -d" " -f1`
-awk '$3>=8.0 {print $1,$2,$3*0.06}' $eqfile | gmt plot -Sa -Gpurple -W0.4p,black
-M8=`awk '$3>=8.0' $eqfile | wc -l | cut -d" " -f1`
+gawk '$3>=5.0 && $3<6.0 {print $1,$2,$3*0.04}' $eqfile | gmt plot -Sc -Gblue
+M5=`gawk '$3>=5.0 && $3<6.0' $eqfile | wc -l | cut -d" " -f1`
+gawk '$3>=6.0 && $3<7.0 {print $1,$2,$3*0.04}' $eqfile | gmt plot -Sc -Gred
+M6=`gawk '$3>=6.0 && $3<7.0' $eqfile | wc -l | cut -d" " -f1`
+gawk '$3>=7.0 && $3<8.0 {print $1,$2,$3*0.04}' $eqfile | gmt plot -Sa -Ggreen
+M7=`gawk '$3>=7.0 && $3<8.0' $eqfile | wc -l | cut -d" " -f1`
+gawk '$3>=8.0 {print $1,$2,$3*0.06}' $eqfile | gmt plot -Sa -Gpurple -W0.4p,black
+M8=`gawk '$3>=8.0' $eqfile | wc -l | cut -d" " -f1`
 
 #绘制图例
 gmt legend -DjBR+w2.5c+l1.2+o0 -F+g229+p0.25p <<EOF
@@ -40,6 +33,4 @@ S 0.4c a 0.42 green 0.25p,black 1c 7~7.9
 C purple
 S 0.4c a 0.48 purple 0.25p,black 1c 8~8.9
 EOF
-
-rm cutTopo*.grd
 gmt end
