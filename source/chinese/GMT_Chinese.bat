76c2f9790eb87de953d7106831e6b6655571ac7c
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2021-12-24 14:08:26 +0900

    update

--- source/chinese/GMT_Chinese.bat
+++ source/chinese/GMT_Chinese.bat
@@ -1,8 +1,5 @@
 gmt begin map pdf,png
 
-gmt set FONT_TITLE 25p,41,black
-gmt set FONT_LABEL 15p,39,black
-
 echo 2 3.5 25p,39,black 中文宋体  > tmp
 echo 2 2.5 25p,40,blue  中文仿宋 >> tmp
 echo 2 1.5 25p,41,red   中文黑体 >> tmp
@@ -12,6 +9,6 @@ echo 5 3.5 25p,44,blue  中文仿宋 >> tmp
 echo 6 3.5 25p,45,red   中文黑体 >> tmp
 echo 7 3.5 25p,46,green 中文楷体 >> tmp
 
-gmt text tmp -R0/8/0/4 -JX12c/4c -Bxaf+l"X轴" -Byaf+l"Y轴" -BWSen+t"中文标题" -F+f
+gmt text tmp -R0/8/0/4 -JX12c/4c -Bxaf -Byaf -F+f
 del tmp
 gmt end
