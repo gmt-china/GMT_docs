1ab34a723418a0ac5c2b4ce05447dbc0ddab4837
王亮 <wangliang0222@gmail.com>
2022-02-15 18:40:50 +0800

    Update plot -L option (#793)
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

---
+++ source/module/plot/plot_-L_1.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+cat << EOF > t.txt
+1 1
+2 3
+3 2
+4 4
+EOF
+cmd='t.txt -R0/5/0/5 -JX4c -W1p -Ba1f1'
+gmt begin plot_-L_1 pdf,png
+gmt plot $cmd -BWStr+t'No -G or -L'
+gmt plot $cmd -BWStr+t'-G Only' -Gorange -X5c
+# -L选项中的+p子选项对画笔的外观设置一律无效
+# 没有-G选项时，使用-L选项必须使用+p子选项，但相关设置无效
+gmt plot $cmd -BWStr+t'-L Only' -L+p10p,blue -X5c
+gmt plot $cmd -BWStr+t'both -G and -L' -Gorange -L+p10p,blue -X5c
+gmt end show
+rm t.txt
---
+++ source/module/plot/plot_-L_2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cat << EOF > t.txt
+1 1 2 4
+2 3 2 3
+3 2 3 2
+4 4 1 1
+EOF
+cmd='t.txt -R0/5/-2/8 -JX5c -W2p -Ba1f1 -Glightred'
+gmt begin plot_-L_2 pdf,png
+gmt plot $cmd -BWStr+t"-L\053d" -L+d
+gmt plot $cmd -BWStr+t"-L\053D" -L+D -X6c
+gmt plot $cmd -BWStr+t"-L\053b" -L+b -X6c
+gmt end
+rm t.txt
