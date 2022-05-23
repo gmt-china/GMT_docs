f7ffd832e2c916027afa5b35aa401f959d795ced
Dongdong Tian <seisman.info@gmail.com>
2022-05-23 12:22:56 +0800

    Apply suggestions from code review
    Co-authored-by: Yao Jiayuan <coreman.seism@gmail.com>

--- source/examples/ex015/ex015.sh
+++ source/examples/ex015/ex015.sh
@@ -14,23 +14,24 @@ gmt colorbar -DjCB+w18c/0.3c+o0/-2.5c+h -BWSEN -Bxa2000f400+l"Elevation/m" -G-80
 # 绘制国界
 gmt plot CN-border-L1.gmt -W1p,-
 
-# 绘制GPS速度场
-# -Se后面两个参数表示：速度值为1的矢量的长度/置信度0.95
+# 绘制 GPS 速度场
+# -Se 后面两个参数表示：
+#    速度值为 1 的矢量的长度（0.05c）/置信度（0.95）
 #    +f 设置文本的大小，0 表示不显示标注
-# -W控制矢量线及误差椭圆的轮廓的宽度,颜色,线型
-# -G矢量头的填充色
-# -A控制矢量的属性，0.15c是矢量头的大小，+e表示在矢量尾端绘制箭头，+p0.75p,blue设置矢量箭头轮廓的属性
-gmt velo gps_campagin.txt -Se0.05c/0.95+f0 -A0.15c+e+p0.5p,blue -Gblue -W0.5p,blue
-gmt velo gps_continuous.txt -Se0.05c/0.95+f0 -A0.15c+e+p0.5p,red -Gred -W0.5p,red
+# -A 控制矢量的属性，0.15c 是矢量头的大小，+e 表示在矢量尾端绘制箭头，+p0.75p,blue 设置矢量箭头轮廓的属性
+# -W 控制矢量线及误差椭圆的轮廓的宽度,颜色,线型
+# -G 矢量头的填充色
+gmt velo gps_campagin.txt -Se0.05c/0.95+f0 -A0.15c+e+p0.5p,blue -W0.5p,blue -Gblue
+gmt velo gps_continuous.txt -Se0.05c/0.95+f0 -A0.15c+e+p0.5p,red -W0.5p,red -Gred
 
 # 添加图例
 # 此处在底图右下角，设置了一个宽 3.5c 高 2c 的底图
 # 底图范围为 0/1/0/1，矢量箭头和文字的位置需对应该小图中的位置
-gmt inset begin -DjBR -F+gwhite -R0/1/0/1 -JX3.5c/2c
+gmt inset begin -R0/1/0/1 -JX3.5c/2c -DjBR -F+gwhite
 echo 0.05 0.75 '20\2611 mm/yr' | gmt text -F+f7p+jML
 echo 0.05 0.50 20 0 1 1 0 | gmt velo -Se0.05c/0.95+f0 -A0.15c+e+p0.5p,blue -Gblue -W0.5p,blue
 echo 0.05 0.25 20 0 1 1 0 | gmt velo -Se0.05c/0.95+f0 -A0.15c+e+p0.5p,red -Gred -W0.5p,red
-gmt text -F+f6p+jML -M <<- EOF
+gmt text -F+f6p+jML -M << EOF
 > 0.4 0.50 0.25 2c c
 Continuous Station
 1998-2014
@@ -38,5 +39,6 @@ Continuous Station
 Campaign Stations
 2009-2014
 EOF
+
 gmt inset end
 gmt end show
