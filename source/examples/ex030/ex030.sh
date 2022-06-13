de8140cee2bd7847eac22cf71762101d865c31a7
Dongdong Tian <seisman.info@gmail.com>
2022-06-13 08:53:19 +0800

    Apply suggestions from code review

--- source/examples/ex030/ex030.sh
+++ source/examples/ex030/ex030.sh
@@ -6,7 +6,7 @@ gmt begin 3d_faultslip
     gmt set MAP_FRAME_TYPE plain #设置边框类型
     gmt set MAP_GRID_PEN 0p,gray,- #设置网格线
     
-    gmt basemap -R97.3/99.5/34.3/35/-25/0 -JX15c/10c -JZ5c -Bxa0.25fg -Bya0.25fg -Bza10f5g5+l"Depth (km)" -BwSEnZ -p160/20
+    gmt basemap -R97.3/99.5/34.3/35/-25/0 -JX15c/10c -JZ5c -Bxa0.25fg -Bya0.25fg -Bza10fg+l"Depth (km)" -BwSEnZ -p160/20
     # 为滑移位错量制作 CPT。滑移位错量最小值为 0， 最大值为 5.5
     gmt makecpt -Cwhite,lightblue,yellow,red -T0/5.5/0.2 -Z
     # 绘制有限断层模型
@@ -15,6 +15,5 @@ gmt begin 3d_faultslip
     # 绘制 colorbar
     gmt set FONT_ANNOT_PRIMARY 7p FONT_LABEL 8p
     gmt set MAP_ANNOT_OFFSET_PRIMARY 2p MAP_FRAME_PEN thinner MAP_TICK_LENGTH 2p
-    
     gmt colorbar -C -DjBL+w2.5c/0.3c+h+o1.3c/2c+ml -Ba1 -Bx+l"slip(cm)"
 gmt end show
