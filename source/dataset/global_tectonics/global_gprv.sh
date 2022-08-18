827096fc59bb08d34834b1e7a424956f5429f26d
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2022-08-18 13:18:06 +0900

    Apply suggestions from code review
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- source/dataset/global_tectonics/global_gprv.sh
+++ source/dataset/global_tectonics/global_gprv.sh
@@ -10,7 +10,7 @@ gmt begin global_gprv
     gmt convert global_gprv.gmt -S"prov_type=passive margin" | gmt plot -G196/233/236 -t30 -l"passive margin"
 
     # 提取出所有和阿尔卑斯-喜马拉雅造山事件相关的地质区域边界数据，并填充红色，设置30%透明度
-    gmt convert global_gprv.gmt -S"lastorogen=Alpine-Himalayan" | gmt plot -Gred -t30 -l"Alpine-Himalayan"
+    gmt convert global_gprv.gmt -S"lastorogen=Alpine-Himalayan" | gmt plot -Gred@30 -l"Alpine-Himalayan"
 
     # 提取出南海盆地的边界数据，绘制出黑色轮廓；提取出鄂尔多斯地块的边界数据，绘制出橘黄色轮廓
     gmt convert global_gprv.gmt -S"prov_name=South China Sea Basins" | gmt plot -W1.5p -l"South China Sea Basins"
