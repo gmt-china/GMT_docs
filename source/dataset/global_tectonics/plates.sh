67b26036a2aec018ca0937db6b8288794818ae4d
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2022-08-18 13:18:17 +0900

    Apply suggestions from code review
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- source/dataset/global_tectonics/plates.sh
+++ source/dataset/global_tectonics/plates.sh
@@ -8,7 +8,7 @@ gmt begin plates
     gmt convert plates.gmt -S"crust_type=continental" | gmt plot -Gorange@80 -l"continental crust plates"
 
     # 从plate_type头段中提取出所有变形带板块，并填充红色，设置透明度50%
-    gmt convert plates.gmt -S"plate_type=deformation zone" | gmt plot -W0p -Gred -L -t50 -l"deformation zone"
+    gmt convert plates.gmt -S"plate_type=deformation zone" | gmt plot -W0p -Gred@50 -L -l"deformation zone"
 
     # 从poly_name头段中提取出青藏高原板块、华北板块，并分别填充蓝色和淡绿色
     gmt convert plates.gmt -S"poly_name=Tibet" | gmt plot -W0.5p -L -Gblue -l"Tibet plate"
