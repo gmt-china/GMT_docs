7a6d43f7e1a9c70077252293e1ba2b9698cd0f29
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2022-08-22 15:22:44 +0900

    Apply suggestions from code review
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- source/examples/ex012/ex012.sh
+++ source/examples/ex012/ex012.sh
@@ -10,9 +10,8 @@ gmt begin mt
     # -Bsxa1O 设置X轴次级标注的间隔为1个月，注意最后一个字符是大写字母O不是数字0
     # -Bpxa7Rf1d 设置X轴主级标注的间隔为7天，刻度间隔为1天
     # -B+l"Date" 设置X轴标签
-    # -Bya1 设置Y轴标注间隔为1
-    # -B+l@%2%M@-L@-@%% 设置Y轴标签，使用2号字体并将L设置为下标
-    gmt basemap -R2017-02-10T00:00:00/2017-05-04T00:00:00/0/5 -JX15c/5c -BSW -Bsxa1O -Bpxa7Rf1d -Bx+l"Date" -Bya1 -By+l@%2%M@-L@-@%%
+    # -Bya1+l 设置Y轴标注间隔为1，并设置Y轴标签，标签使用斜体（2号字体），L 为下标
+    gmt basemap -R2017-02-10T00:00:00/2017-05-04T00:00:00/0/5 -JX15c/5c -BSW -Bsxa1O -Bpxa7Rf1d -Bx+l"Date" -Bya1+l@%2%M@-L@-@%%
     
     # -Sb0.005c 中 0.005c 代表每条线的宽度
     gmt plot mt.txt -Sb0.005c -Gblack 
