32af7c479ebdf61f5fd1871c5214d95bb3f30344
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2022-10-03 13:05:44 +0900

    Fix plot_-L_3.sh (#984)

--- source/module/plot/plot_-L_3.sh
+++ source/module/plot/plot_-L_3.sh
@@ -10,7 +10,7 @@ gmt begin plot_-L_3
 gmt plot $cmd -BWStr+t"-L\053xl" -L+xl
 gmt plot $cmd -BWStr+t"-L\053xr" -L+xr -X6c
 gmt plot $cmd -BWStr+t"-L\053x4.5" -L+x4.5 -X6c
-gmt plot $cmd -BWStr+t"-L\053yt" -L+yt -X-12c -Y-6.5c
+gmt plot $cmd -BWStr+t"-L\053yt" -L+yt -X-12c -Y6.5c
 gmt plot $cmd -BWStr+t"-L\053yb" -L+yb -X6c
 gmt plot $cmd -BWStr+t"-L\053y4" -L+y4 -X6c
 gmt end show
