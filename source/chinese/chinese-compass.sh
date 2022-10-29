48569dae4548754d0132bf4d705f4085fdac5ac7
zhou mao <1831326353@qq.com>
2022-10-29 19:27:17 +0800

    Add Chinese rose showcases

---
+++ source/chinese/chinese-compass.sh
@@ -0,0 +1,15 @@
+gmt begin chinese-compass png
+
+# 设置GMT语言为中文
+gmt set GMT_LANGUAGE cn1
+
+# 设置标注的字体为中文字体
+gmt set FONT_TITLE 12p,39
+
+    # 方向玫瑰图
+	gmt basemap -R-7/7/-5/5 -Tdg0/0+w0.6i+f3+l+jCM
+    
+    # 磁场玫瑰图
+	gmt basemap -R-7/7/-5/5 -Tmg0/0+w1i+d-14.5+t45/10/5+i0.25p,blue+p0.25p,red+l+jCM -X1.5i 
+
+gmt end show
