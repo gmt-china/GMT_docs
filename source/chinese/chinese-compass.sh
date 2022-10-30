f3d5b46df21f601b136cc649c88b259e3a38cea7
zhou mao <1831326353@qq.com>
2022-10-30 10:26:57 +0800

    Fix

--- source/chinese/chinese-compass.sh
+++ source/chinese/chinese-compass.sh
@@ -7,9 +7,9 @@ gmt set GMT_LANGUAGE cn1
 gmt set FONT_TITLE 12p,39
 
     # 方向玫瑰图
-	gmt basemap -R-7/7/-5/5 -Tdg0/0+w0.6i+f3+l+jCM
+    gmt basemap -R-7/7/-5/5 -Tdg0/0+w0.6i+f3+l+jCM
     
     # 磁场玫瑰图
-	gmt basemap -R-7/7/-5/5 -Tmg0/0+w1i+d-14.5+t45/10/5+i0.25p,blue+p0.25p,red+l+jCM -X1.5i 
+    gmt basemap -R-7/7/-5/5 -Tmg0/0+w1i+d-14.5+t45/10/5+i0.25p,blue+p0.25p,red+l+jCM -X1.5i 
 
 gmt end show
