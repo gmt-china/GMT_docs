e658f9c9df5020d3086b23af6975b9214a4d2f5e
Dongdong Tian <seisman.info@gmail.com>
2023-10-04 17:43:43 +0800

    fix

--- source/dataset/global_tectonics/oc_boundaries.sh
+++ source/dataset/global_tectonics/oc_boundaries.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 gmt begin oc_boundaries
-	gmt basemap -JN15c -Rg -Ba60
-	gmt coast -G244/243/239 -S167/194/223
-	gmt plot oc_boundaries.gmt -W0.5p,red
+    gmt basemap -JN15c -Rg -Ba60
+    gmt coast -G244/243/239 -S167/194/223
+    gmt plot oc_boundaries.gmt -W0.5p,red
 gmt end show
