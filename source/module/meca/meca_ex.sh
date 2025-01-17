51125c5d8e54c9dbc13475b1084087c366bec19c
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-01-17 19:24:58 +0900

    Update meca_ex.sh

--- source/module/meca/meca_ex.sh
+++ source/module/meca/meca_ex.sh
@@ -5,6 +5,6 @@ gmt begin meca_ex
     # 
     gmt basemap -R-75.5/-63/-35/-30 -JM10c -Baf
     gmt coast -G244/243/239 -S167/194/223
-    gmt makecpt -T0/150/1 -Z
-    gmt meca GCMT_1976-2017_meca.gmt -Sc0.2c+f0 -C
+    gmt makecpt -T0/150/1 -Z -D
+    gmt meca GCMT_1976-2017_meca.gmt -Fa -Sc0.2c+f0 -C
 gmt end show
