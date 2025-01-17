19ff0575ec242d931c11996a68cf6c3699875f0d
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-01-17 19:11:28 +0900

    Update meca_ex.sh

--- source/module/meca/meca_ex.sh
+++ source/module/meca/meca_ex.sh
@@ -3,8 +3,8 @@ gmt begin meca_ex
     # 从GMT远程服务器下载示例地震目录文件
     gmt which -Gl @GCMT_1976-2017_meca.gmt
     # 
-    gmt basemap -R-75.1/-63/-34.44/-30.35 -JM10c -Baf
+    gmt basemap -R-75.5/-63/-35/-30 -JM10c -Baf
     gmt coast -G244/243/239 -S167/194/223
     gmt makecpt -T0/150/1 -Z
-    gmt meca GCMT_1976-2017_meca.gmt -Sc0.5c+f0 -C
+    gmt meca GCMT_1976-2017_meca.gmt -Sc0.2c+f0 -C
 gmt end show
