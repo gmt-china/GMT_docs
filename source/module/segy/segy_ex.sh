d3e9d82988788c22ac6e30f51020ba6bd6a93b17
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-01-18 18:31:00 +0900

    Update segy_ex.sh

--- source/module/segy/segy_ex.sh
+++ source/module/segy/segy_ex.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 gmt begin segy1
-    gmt segy XB1-Z.sgy -JX5i/-5i -R0/100/0/10 -D1 -C3 -N -So -W -Fblack
+    gmt segy XB1-Z.sgy -JX5i/-5i -R0/30/0/10 -Baf -D -N -W
 gmt end show
