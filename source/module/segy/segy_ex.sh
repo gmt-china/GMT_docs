d15d294e6fd4c031a1938dd680421f105fb3049e
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-01-18 20:20:05 +0900

    Update segy_ex.sh

--- source/module/segy/segy_ex.sh
+++ source/module/segy/segy_ex.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 gmt begin segy1
-    gmt basemap -JX5i/-5i -R0/34/0/3 -Bxaf+ltraces -Byaf+l"t (s)"
+    gmt basemap -JX5i/-5i -R0/33/0/3 -Bxaf+ltraces -Byaf+l"t (s)"
     gmt segy XB1-Z.sgy -D0.1 -N -W
 gmt end show
