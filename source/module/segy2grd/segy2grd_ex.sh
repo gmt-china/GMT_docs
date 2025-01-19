4bae3d903fa0faa4a88795e05162b432030614cf
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-01-19 09:45:17 +0900

    Update segy2grd_ex.sh

--- source/module/segy2grd/segy2grd_ex.sh
+++ source/module/segy2grd/segy2grd_ex.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+# 下载示例数据
+wget -c https://github.com/CovMat/example-figure-data/raw/refs/heads/main/Marmousi.sgy
+gmt begin ex
+    #
+    gmt basemap -R1/2801/0/1.4
+gmt end show
