ee620598672ea734b590b2700e4af3141ba68690
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-01-21 19:39:40 +0900

    Update ex010.sh

--- source/examples/ex010/ex010.sh
+++ source/examples/ex010/ex010.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 gmt begin ex010
     # 获取发震时刻最大最小值
-    start=$(gmt info data.txt -C -o0 -i0 | tr -d '\n')
-    end=$(gmt info data.txt -C -o1 -i0 | tr -d '\n')
+    # 以 -Tzmin/zmax/dz 的形式报告第一列，每四周一个间隔dz
+    T_cpt=$(gmt info data.txt -C -T4w+c0)
     
     # 根据发震时刻绘制不同颜色的圆点
-    gmt makecpt -Chot -T${start}/${end}/100+n -Z -Di
+    gmt makecpt -Chot ${T_cpt} -Z -Di
     gmt plot data.txt -Sc0.1c -W -C -Ra -JM15c -Baf -i1,2,0
     gmt colorbar -Bxaf -C
     # 绘制省界，断层
