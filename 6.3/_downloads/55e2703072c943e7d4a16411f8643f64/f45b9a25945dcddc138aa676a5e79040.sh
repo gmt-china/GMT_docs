214bfa5774a12dd3bc441351e98e7053c94ca52c
seisman <seisman@users.noreply.github.com>
2025-04-01 23:12:20 +0000

    deploy: 9d9cbd84d5c958f076d597475bed9bef14e76e48

---
+++ 6.3/_downloads/55e2703072c943e7d4a16411f8643f64/f45b9a25945dcddc138aa676a5e79040.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+gmt begin B_linear
+gmt basemap -R0/12/0/0.95 -JX3i/0.3i -Ba4f2g1+lFrequency+u" %" -BS
+gmt plot -Sv2p+e+a60 -W0.5p -Gblack -Y0.1i -N << EOF
+2 0 0 0.5i
+2 0 180 0.5i
+7 0 0 0.25i
+7 0 180 0.25i
+9.5 0 0 0.125i
+9.5 0 180 0.125i
+EOF
+gmt text -Gwhite -C0.01i/0.01i -F+f9p+jCB << EOF
+2 0.2 annotation
+7 0.2 frame
+9.5 0.2 grid
+EOF
+gmt end show
