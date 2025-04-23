ca022d4b8137578040f3dca4cd42155ab7be428e
Dongdong Tian <seisman.info@gmail.com>
2025-04-23 14:15:55 +0800

    Fix

--- source/examples/ex013/ex013.sh
+++ source/examples/ex013/ex013.sh
@@ -4,12 +4,12 @@
 #
 
 gmt begin ex013
-	# Clip the Anhui province
-	gmt coast -ECN.AH+c -RCN.AH -JM10c
-	# Plot the earth relief
-	gmt grdimage @earth_relief_15s -I+d
-	# Add rivers and lakes
-	gmt coast -Ia/1p,lightblue -Slightblue
-	# Undo the clipping
-	gmt clip -C
+    # Clip the Anhui province
+    gmt coast -ECN.AH+c -RCN.AH -JM10c
+    # Plot the earth relief
+    gmt grdimage @earth_relief_15s -I+d
+    # Add rivers and lakes
+    gmt coast -Ia/1p,lightblue -Slightblue
+    # Undo the clipping
+    gmt clip -C
 gmt end show
