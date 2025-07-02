771c7800a5221b5c020899ede041b3282c4f2b8b
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-07-02 15:19:07 +0900

    MAP_FRAME_TYPE: Add description for graph-origin (#1487)

--- source/conf/map_frame_type.sh
+++ source/conf/map_frame_type.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 gmt begin map_frame_type
 gmt set FONT_TITLE 18p
-gmt subplot begin 1x5 -Fs4c/4c -M0.5c/0.2c
+gmt subplot begin 2x3 -Fs4c/4c -M0.5c/0.8c
 gmt basemap -JM? -R0/10/0/10 -Baf -B+t"fancy" -c --MAP_FRAME_TYPE=fancy
 gmt basemap -JM? -R0/10/0/10 -Baf -B+t"fancy+" -c --MAP_FRAME_TYPE=fancy+
 gmt basemap -JX? -R0/10/0/10 -Baf -BWSen+t"plain" -c --MAP_FRAME_TYPE=plain
 gmt basemap -JX? -R0/10/0/10 -Baf -BWSen+t"inside" -c --MAP_FRAME_TYPE=inside
 gmt basemap -JX? -R0/10/0/10 -Baf -BWS+t"graph" -c --MAP_FRAME_TYPE=graph
+gmt basemap -JX? -R-5/5/-5/5 -Baf -BWS+t"graph-origin" -c --MAP_FRAME_TYPE=graph-origin
 gmt subplot end
 gmt end show
