2fd2650acb9a805f40a6c06aef6731bfb25574f0
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-07-30 21:58:22 +0900

    Update spilhaus.sh

--- source/option/J/spilhaus.sh
+++ source/option/J/spilhaus.sh
@@ -1,4 +1,4 @@
 gmt begin spilhaus
-    gmt grdimage @earth_relief_10m_p -Rd -J+proj=spilhaus+width=15c -I+d -Cgeo
-    gmt coast -BWbet -B30g -A10000 -Wthinnest
+    gmt grdimage @earth_relief_10m_p -Rd -J+proj=spilhaus+width=10c -I+d -Cgeo
+    gmt coast -BWbet -B60g -A10000 -Wthinnest
 gmt end show
