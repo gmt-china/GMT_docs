23bea899bf4cef483852b237d803cf09111076d4
陈箫翰 <26203721+CovMat@users.noreply.github.com>
2025-08-15 16:44:13 +0900

    Update ex003.sh

--- source/examples/ex003/ex003.sh
+++ source/examples/ex003/ex003.sh
@@ -3,19 +3,24 @@
 # 以地形起伏进行明暗处理绘制其它数据
 #
 gmt begin relief-shaded-image
-    gmt subplot begin 1x2 -Fs10c/10c -M0.5c
+    gmt subplot begin 2x1 -Fs10c/10c -Baf
 
     # 未经地形起伏调制的洋壳年龄数据
     gmt subplot set 0
-    gmt grdimage @earth_age_06m -R150/250/-40/40 -B+t"Original crustal age"
-    gmt coast -W0.5p -Baf
-    gmt colorbar -Baf50
+    # 根据用户实际需求先绘制下方图层，代码略
+    # ...
+    # -Q 选项可以让没有洋壳年龄数据的区域透明，显示出下方的图层
+    gmt grdimage @earth_age_06m -R150/250/-40/40 -B+t"Original crustal age" -Q
+    gmt coast -W0.5p
 
     # 使用地形起伏进行明暗处理的洋壳年龄数据
     gmt subplot set 1
-    gmt grdimage @earth_age_06m -R150/250/-40/40 -I@earth_relief_06m+d -B+t"Relief-shaded crustal age"
-    gmt coast -W0.5p -Baf
-    gmt colorbar -Baf50
+    # 根据用户实际需求先绘制下方图层，代码略
+    # ...
+    # -Q 选项可以让没有洋壳年龄数据的区域透明，显示出下方的图层
+    gmt grdimage @earth_age_06m -R150/250/-40/40 -I@earth_relief_06m+d -B+t"Relief-shaded crustal age" -Q
+    gmt coast -W0.5p
+    gmt colorbar -Baf50+l"crustal age (Myr)"
 
     gmt subplot end
 gmt end show
