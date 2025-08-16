125f53b1824e10008f739325d07e24eca017058d
陈箫翰 <26203721+CovMat@users.noreply.github.com>
2025-08-16 20:28:38 +0900

    update -B  (#1508)

---
+++ source/option/B/B_time_axes_1.sh
@@ -0,0 +1,4 @@
+gmt begin GMT_-B_time1
+    gmt set FORMAT_DATE_MAP=-o FONT_ANNOT_PRIMARY +9p
+    gmt basemap -R2000-4-1T/2000-5-25T/0/1 -JX5i/0.2i -Bpxa7Rf1d -Bsxa1O -BS
+gmt end show
---
+++ source/option/B/B_time_axes_6.sh
@@ -0,0 +1,4 @@
+gmt begin GMT_-B_time6
+    gmt set FORMAT_DATE_MAP "o yy" FORMAT_TIME_PRIMARY_MAP Abbreviated
+    gmt basemap -R1996T/1996-6T/0/1 -JX5i/0.2i -Bxa1Of1d -BS
+gmt end show
