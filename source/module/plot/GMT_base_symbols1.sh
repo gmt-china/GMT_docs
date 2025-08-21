aa6893db6ea608aa5a096c8b8cde2ada60ae1a21
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-08-22 06:37:38 +0900

    update

--- source/module/plot/GMT_base_symbols1.sh
+++ source/module/plot/GMT_base_symbols1.sh
@@ -4,7 +4,6 @@
 
 gmt begin GMT_base_symbols1
 cat << EOF > tmp.txt
-# All the basic geometric psxy symbols
 1	2	1c	a
 2	2	1c	c
 3	2	1c	d
@@ -22,6 +21,6 @@ cat << EOF > tmp.txt
 EOF
     gmt plot tmp.txt -R0.6/7.4/0.6/2.4 -B0g1 -B+n -Jx1.5c -Sc1c -W0.25p --PROJ_LENGTH_UNIT=cm -i0,1 --MAP_GRID_PEN_PRIMARY=default,dashed
     gmt plot tmp.txt -S -Glightblue -W1p --PROJ_LENGTH_UNIT=cm
-    $AWK '{if ($2 == 1) print $1, $2, $NF}' tmp.txt | gmt text -N -F+f10p,Helvetica-Bold+jTC -Dj0/0.75c
-    $AWK '{if ($2 == 2) print $1, $2, $NF}' tmp.txt | gmt text -N -F+f10p,Helvetica-Bold+jBC -Dj0/0.75c
+    gawk '{if ($2 == 1) print $1, $2, $NF}' tmp.txt | gmt text -N -F+f10p,Helvetica-Bold+jTC -Dj0/0.75c
+    gawk '{if ($2 == 2) print $1, $2, $NF}' tmp.txt | gmt text -N -F+f10p,Helvetica-Bold+jBC -Dj0/0.75c
 gmt end show
