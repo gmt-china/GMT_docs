55b167ef6a1583162fe12ee008daf602c3164083
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-08-22 05:11:33 +0900

    update

--- source/module/plot/plot_symbols.sh
+++ source/module/plot/plot_symbols.sh
@@ -1,33 +1,27 @@
 #!/usr/bin/env bash
-gmt begin plot_symbols
-gmt plot -JX20c/2c -R0.5/13.5/0/4 -B1 -BS -S0.5c << EOF
-1 3 -
-2 3 +
-3 3 a
-4 3 c
-5 3 d
-6 3 g
-7 3 h
-8 3 i
-9 3 n
-10 3 s
-11 3 t
-12 3 x
-13 3 y
+#
+# Plot psxy basic single-parameter geometric symbols for use on man page
+
+gmt begin GMT_base_symbols1
+cat << EOF > tmp.txt
+# All the basic geometric psxy symbols
+1	2	1c	a
+2	2	1c	c
+3	2	1c	d
+4	2	1c	g
+5	2	1c	h
+6	2	1c	i
+7	2	1c	n
+1	1	1c	p
+2	1	1c	s
+3	1	1c	t
+4	1	1c	x
+5	1	1c	y
+6   1   1c  -
+7   1   1c  +
 EOF
-gmt text -F+f15p,9 << EOF
-1 1 -S-
-2 1 -S+
-3 1 -Sa
-4 1 -Sc
-5 1 -Sd
-6 1 -Sg
-7 1 -Sh
-8 1 -Si
-9 1 -Sn
-10 1 -Ss
-11 1 -St
-12 1 -Sx
-13 1 -Sy
-EOF
+    gmt plot tmp.txt -R0.6/7.4/0.6/2.4 -B0g1 -B+n -Jx1.5c -Sc1c -W0.25p --PROJ_LENGTH_UNIT=cm -i0,1 --MAP_GRID_PEN_PRIMARY=default,dashed
+    gmt plot tmp.txt -S -Glightblue -W1p --PROJ_LENGTH_UNIT=cm
+    $AWK '{if ($2 == 1) print $1, $2, $NF}' tmp.txt | gmt text -N -F+f10p,Helvetica-Bold+jTC -Dj0/0.75c
+    $AWK '{if ($2 == 2) print $1, $2, $NF}' tmp.txt | gmt text -N -F+f10p,Helvetica-Bold+jBC -Dj0/0.75c
 gmt end show
