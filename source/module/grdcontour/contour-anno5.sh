ed0ee8a20d2a0243484815eb1d50ad7ae3a6b529
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-08-25 03:00:57 +0900

    update

--- source/module/grdcontour/contour-anno5.sh
+++ source/module/grdcontour/contour-anno5.sh
@@ -22,5 +22,7 @@ EOF
 gmt begin contour_annot5
     gmt coast -R50/160/-15/15 -JM5.3i -Gburlywood -Sazure -A500
     # -GXcross.txt：从 cross.txt 读取折线数据，在折线与等值线的交点处标注
-    gmt grdcontour @App_O_geoid.nc -B20f10 -BWSne -C10 -A20+f8p -GXcross.txt -S10 -T+lLH
+    # 在该连线与等值线的交点上进行标注
+    # 使用 -A 中的 +d 附加选项会绘制出连线。如果不想绘制连线可以去掉 +d
+    gmt grdcontour @App_O_geoid.nc -B20f10 -BWSne -C10 -A20+f8p+d -GXcross.txt -S10 -T+lLH
 gmt end show
