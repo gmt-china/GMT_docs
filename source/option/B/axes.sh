c486098aa1d1e923c3420bfcc1211cac75f229e1
陈箫翰 <26203721+CovMat@users.noreply.github.com>
2025-09-30 14:46:57 +0900

    Update axes.sh

--- source/option/B/axes.sh
+++ source/option/B/axes.sh
@@ -1,6 +1,6 @@
 gmt begin B-axes
 gmt set FONT_TITLE 20p MAP_FRAME_PEN 3p
-gmt basemap -R0/4/0/4 -JX8c -BWSEN+t"2D Basemap" -Xc
+gmt basemap -R0/4/0/4 -JX8c -BWSEN+t"2D Basemap"
 gmt text -F+f15p,9 -Glightblue@30 -N << EOF
 0 2 W,w,l
 4 2 E,e,r
@@ -8,7 +8,7 @@ gmt text -F+f15p,9 -Glightblue@30 -N << EOF
 2 0 S,s,b
 EOF
 
-gmt basemap -R0/4/0/4/0/4 -JX5c -JZ5c -Bwsenz1234+b+t"3D Basemap" -X12c -p130/30
+gmt basemap -R0/4/0/4/0/4 -JX5c -JZ5c -Bwsenz1234+b+t"3D Basemap" -X10c -p130/30
 gmt text -F+f15p,9 -JZ -Glightblue@30 -Z -N -p << EOF
 0 2 0 W,w,l
 4 2 0 E,e,r
