146fe620dc2a7e88b53b072cd28b8588c4b0e724
Xiaohan Chen <26203721+CovMat@users.noreply.github.com>
2025-10-18 18:17:22 +0900

    Update ex029_1.sh

--- source/examples/ex029/ex029_1.sh
+++ source/examples/ex029/ex029_1.sh
@@ -4,7 +4,7 @@
 gmt begin 3D_earth_relief_1
     gmt set MAP_FRAME_TYPE plain
     # 1. 绘制地形图形，图层默认位于 z 轴底部（此例中即 -1000 的高度）
-    gmt grdview @earth_relief_06m -R101/108/35/42/-1000/20000 -JM10c -JZ10c -N-1000+ggray -Qi -I -Ba -BwsENZ -p60/25    
+    gmt grdview @earth_relief_06m -R101/108/35/42/-1000/20000 -JM10c -JZ10c -N-1000+ggray -Qs -I+d -Ba -BwsENZ -p60/25    
     gmt colorbar -C -Ba -DJTC+o0/1c -p
 
     # 2. 绘制断层线图层，-p 设置图层抬高到10000 
