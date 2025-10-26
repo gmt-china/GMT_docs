68101ee82e05baa380ce9a0a2a5c07149f0fe436
He Xingchen <chuanjun1978@gmail.com>
2025-10-26 12:59:25 +0800

    Update source/examples/ex032/ex032.sh
    Co-authored-by: Mao Zhou <maozhougeo@gmail.com>

--- source/examples/ex032/ex032.sh
+++ source/examples/ex032/ex032.sh
@@ -1,29 +1,18 @@
 #!/usr/bin/env bash
 #
-#带岛屿的湖泊填充示例
-#
-DEM_FILE="taihu_highres.nc"    # 高精度数字高程模型（DEM）文件
 LAKE_FILE="taihu.gmt"          # 太湖湖泊边界的 GMT 格式文件
 
-# 下载高精度 DEM（3 秒分辨率）
-gmt grdcut @earth_relief_03s -R119/121/30/32 -G"$DEM_FILE"
-
-# 绘制子图（左图：轮廓，右图：填充）
 gmt begin taihu png
 
-    gmt subplot begin 1x2 -Fs6i/6i -A -M0.5i -R119/121/30/32 -JX6i/6i
+    gmt subplot begin 1x2 -Fs6i/6i -A -M0.5i -R119/121/30/32 -JM6i
 
-        # 左图：绘制湖泊轮廓
         gmt subplot set 0                 
-        gmt grdimage "$DEM_FILE" -Cgeo 
+        gmt grdimage @earth_relief_15s -Cgeo -B+t"Taihu Lake Outline"
         gmt plot "$LAKE_FILE" -W0.4p,red    # 绘制红色湖泊轮廓线
-        gmt basemap -B+t"Taihu Lake Outline"
 
-        # 右图：绘制湖泊填充
         gmt subplot set 1
-        gmt grdimage "$DEM_FILE" -Cgeo
+        gmt grdimage @earth_relief_15s -Cgeo -B+t"Taihu Lake Fill"
         gmt plot "$LAKE_FILE" -Gdeepskyblue  # 填充湖泊为深蓝色
-        gmt basemap -B+t"Taihu Lake Fill"
 
     gmt subplot end  
 gmt end show        
