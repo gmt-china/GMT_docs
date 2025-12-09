f65eab46d2778e36e3674e96beb9f397ab2dcdae
陈箫翰 <26203721+CovMat@users.noreply.github.com>
2025-12-09 20:43:29 +0900

    Enhance GMT_Chinese.sh with additional comments

--- source/chinese/GMT_Chinese.sh
+++ source/chinese/GMT_Chinese.sh
@@ -2,6 +2,8 @@
 gmt begin GMT_Chinese png,pdf
 # 设置中文字体配置文件 cidfmap 的目录
 gmt set PS_CONVERT="C-I${HOME}/.gmt/"
+# 如果有其他需要传递给 gmt psconvert 命令的选项，例如 -I+m0.5c -E720，可以去掉横杆后，以逗号分隔添加在后面
+# 例如：gmt set PS_CONVERT="C-I${HOME}/.gmt/,I+m0.5c,E720"
 
 # GMT 处理中文存在一些已知BUG
 # 需要设置 PS_CHAR_ENCODING 为 Standard+ 以绕过这一BUG
