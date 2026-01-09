671a34f1663b50da700d584d948852fb82c0238f
chuan <chuanjun1978@gmail.com>
2026-01-09 16:33:30 +0800

    Refactor: Use font names instead of IDs for Chinese font support

--- source/chinese/GMT_Chinese.bat
+++ source/chinese/GMT_Chinese.bat
@@ -2,19 +2,19 @@ gmt begin map pdf,png
 REM GMT处理中文存在一些已知BUG
 REM 需要设置 PS_CHAR_ENCODING 为 Standard+ 以绕过这一BUG
 gmt set PS_CHAR_ENCODING Standard+
-gmt set FONT_TITLE 25p,41,black
-gmt set FONT_LABEL 15p,39,black
+gmt set FONT_TITLE 25p,STHeiti-Regular--GB-EUC-H,black
+gmt set FONT_LABEL 15p,STSong-Light--GB-EUC-H,black
 
 REM GMT 6.3 及之后的版本连续使用echo命令输出含中文的文件时，必须在开始设置 chcp 936，即 GB 编码
 chcp 936
-echo 2 3.5 25p,39,black 中文宋体  > tmp
-echo 2 2.5 25p,40,blue  中文仿宋 >> tmp
-echo 2 1.5 25p,41,red   中文黑体 >> tmp
-echo 2 0.5 25p,42,green 中文楷体 >> tmp
-echo 4 3.5 25p,43,black 中文宋体 >> tmp
-echo 5 3.5 25p,44,blue  中文仿宋 >> tmp
-echo 6 3.5 25p,45,red   中文黑体 >> tmp
-echo 7 3.5 25p,46,green 中文楷体 >> tmp
+echo 2 3.5 25p,STSong-Light--GB-EUC-H,black 中文宋体  > tmp
+echo 2 2.5 25p,STFangsong-Light--GB-EUC-H,blue  中文仿宋 >> tmp
+echo 2 1.5 25p,STHeiti-Regular--GB-EUC-H,red   中文黑体 >> tmp
+echo 2 0.5 25p,STKaiti-Regular--GB-EUC-H,green 中文楷体 >> tmp
+echo 4 3.5 25p,STSong-Light--GB-EUC-V,black 中文宋体 >> tmp
+echo 5 3.5 25p,STFangsong-Light--GB-EUC-V,blue  中文仿宋 >> tmp
+echo 6 3.5 25p,STHeiti-Regular--GB-EUC-V,red   中文黑体 >> tmp
+echo 7 3.5 25p,STKaiti-Regular--GB-EUC-V,green 中文楷体 >> tmp
 
 REM GMT 6.3 及之后的版本每句使用中文的命令之前必须设置 chcp 936，即 GB 编码
 chcp 936
--- source/chinese/GMT_Chinese.sh
+++ source/chinese/GMT_Chinese.sh
@@ -3,17 +3,17 @@ gmt begin GMT_Chinese png,pdf C-I${HOME}/.gmt/
 # GMT 处理中文存在一些已知BUG
 # 需要设置 PS_CHAR_ENCODING 为 Standard+ 以绕过这一BUG
 gmt set PS_CHAR_ENCODING Standard+
-gmt set FONT_TITLE 25p,41,black
-gmt set FONT_LABEL 15p,39,black
+gmt set FONT_TITLE 25p,STHeiti-Regular--UniGB-UTF8-H,black
+gmt set FONT_LABEL 15p,STSong-Light--UniGB-UTF8-H,black
 
 gmt text -R0/8/0/4 -JX12c/4c -Bxaf+l"X轴" -Byaf+l"Y轴" -BWSen+t"中文标题" -F+f << EOF
-2 3.5 25p,39,black 中文宋体
-2 2.5 25p,40,blue  中文仿宋
-2 1.5 25p,41,red   中文黑体
-2 0.5 25p,42,green 中文楷体
-4 3.5 25p,43,black 中文宋体
-5 3.5 25p,44,blue  中文仿宋
-6 3.5 25p,45,red   中文黑体
-7 3.5 25p,46,green 中文楷体
+2 3.5 25p,STSong-Light--UniGB-UTF8-H,black 中文宋体
+2 2.5 25p,STFangsong-Light--UniGB-UTF8-H,blue  中文仿宋
+2 1.5 25p,STHeiti-Regular--UniGB-UTF8-H,red   中文黑体
+2 0.5 25p,STKaiti-Regular--UniGB-UTF8-H,green 中文楷体
+4 3.5 25p,STSong-Light--UniGB-UTF8-V,black 中文宋体
+5 3.5 25p,STFangsong-Light--UniGB-UTF8-V,blue  中文仿宋
+6 3.5 25p,STHeiti-Regular--UniGB-UTF8-V,red   中文黑体
+7 3.5 25p,STKaiti-Regular--UniGB-UTF8-V,green 中文楷体
 EOF
 gmt end show
